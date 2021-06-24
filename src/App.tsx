87322fd05246cb55b79af9302b99e71cacab0c18
Milena Ferraz <milenaferraz@gmail.com>
2021-06-24 09:47:18 -0300

    aula-02

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Button } from './components/Button'
+import { Route, BrowserRouter } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom'
+import { AuthContextProvider } from './contexts/AuthContext'
 
-function App() {
+function App() { 
+
   return (
-    <div>
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>      
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/rooms/new" component={NewRoom}></Route>      
+      </ AuthContextProvider>
+    </BrowserRouter>   
+    
   );
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react"
+import { ButtonHTMLAttributes } from 'react';
 
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button() {
-
-    const [counter, setCounter] = useState(0);
-
-    function increment() {
-        setCounter(counter + 1)        
-    }
-
+export function Button(props: ButtonProps) {
+ 
     return (
-        <button onClick={increment}>{counter}</button>
+        <button className="button" {...props}></button>
     )
 
 }
